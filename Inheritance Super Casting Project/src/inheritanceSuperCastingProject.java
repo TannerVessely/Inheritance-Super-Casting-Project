833648748d40b7fa7dd09db09050bf94f8b15297
tanner.vessely <tanner.vessely@MU-C1-10.mullen.pvt.k12.co.us>
2025-02-13 13:14:31 -0700

    initial commit

---
+++ Inheritance Super Casting Project/src/inheritanceSuperCastingProject.java
@@ -0,0 +1,18 @@
+
+public class inheritanceSuperCastingProject
+	{
+
+		public static void main(String[] args)
+			{
+				// TODO Auto-generated method stub
+
+			}
+	
+
+public class StreatRacing extends Driving
+		
+		{
+			
+		}+-
+		
+	}
---
+++ Inheritance Super Casting Project/src/streetRacing.java
@@ -0,0 +1,5 @@
+
+public class streetRacing extends driving
+	{
+
+	}
