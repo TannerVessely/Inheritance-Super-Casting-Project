5875098e365f1342643c9910924ed3010c6ef90e
Tanne <Tanne@192.168.1.111>
2025-02-17 20:58:26 -0700

    homecommit

--- Inheritance Super Casting Project/src/inheritanceSuperCastingProject.java
+++ Inheritance Super Casting Project/src/inheritanceSuperCastingProject.java
@@ -1,10 +1,23 @@
+// Abstract class driving
+abstract class driving {
+    // Instance variables
+    String vehicleType;
+    int maxSpeed;
 
-public class inheritanceSuperCastingProject
-	{
 
-		public static void main(String[] args)
-			{
-				// TODO Auto-generated method stub
+    public driving(String vehicleType, int maxSpeed) {
+        this.vehicleType = vehicleType;
+        this.maxSpeed = maxSpeed;
+    }
 
-			}
-	}
+
+    public void startEngine() {
+        System.out.println("The engine is starting...");
+    }
+  public abstract void drive();
+
+  public void displayInfo() {
+        System.out.println("Vehicle Type: " + vehicleType);
+        System.out.println("Max Speed: " + maxSpeed + " km/h");
+    }
+}
